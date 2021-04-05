335008572008d2a35a1acb88436b703eb8c399ac
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-04-05 16:36:02 -0400

    smoother margins

--- canvas.js
+++ canvas.js
@@ -1,38 +1,20 @@
-let num_nodes = 700;
+let num_nodes = 100;
 let node_speed = .05;
+
+let node_rad = 5;
+
 let nodes = [];
 
 let canvas;
 let size_scaler = 1;
 
-function get_scale() {
-  return min(windowWidth / canvas.width, windowHeight / canvas.height);
-}
-
-function windowResized() {
-  //scale window
-  size_scaler = get_scale();
-  canvas = createCanvas(
-    screen.width * size_scaler,
-    screen.height * size_scaler,
-  );
-  canvas.position(
-    windowWidth / 2 - (canvas.width) / 2,
-    0,
-    "fixed",
-  );
-
-  scale(size_scaler);
-}
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-  size_scaler = get_scale();
   canvas.position(
     windowWidth / 2 - (canvas.width * size_scaler) / 2,
     0,
     "fixed",
   );
-  scale(size_scaler);
   background(0);
 
   for (let i = 0; i < num_nodes; i++) {
@@ -44,14 +26,14 @@ function setup() {
       random(-node_speed, node_speed),
       node_speed,
     );
-    nodes.push(new node(x, y, vel));
+    nodes.push(new node(x, y, vel, node_rad));
   }
 }
 function draw() {
   background(0);
   fill(255);
   nodes.forEach((n) => {
-    ellipse(n.x, n.y, 10, 10);
-    n.move(canvas, size_scaler);
+    n.display();
+    n.move(canvas);
   });
 }
--- nodes.js
+++ nodes.js
@@ -1,20 +1,27 @@
 class node {
-  constructor(x, y, vel) {
+  constructor(x, y, vel, rad) {
     this.x = x;
     this.y = y;
     this.vel = vel;
+    this.rad = rad;
 
+    this.color = (255, 255, 255);
     this.move_updated = Date.now();
   }
+  display() {
+    fill(this.color);
+    ellipse(this.x, this.y, this.rad * 2);
+  }
+
   move(c) {
     this.x += this.vel[0] * (Date.now() - this.move_updated);
     this.y += this.vel[1] * (Date.now() - this.move_updated);
     this.move_updated = Date.now();
 
-    if (this.x < 0) {
-      this.x = c.width;
-    } else if (this.x > c.width) {
-      this.x = 0;
+    if (this.x < -this.rad) {
+      this.x = c.width + this.rad;
+    } else if (this.x > c.width + this.rad) {
+      this.x = -this.rad;
     }
 
     if (this.y < 0) {
